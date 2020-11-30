04a6c591f8f948427e49fdb3b6b723aa4a21afbf
Imen EL HAKIM <imen.elhakim@esprit.tn>
2020-11-30 02:02:21 +0100

    Commit 2 : + 4 components : Event / PageNotFound / Product / User. + 1 directory : model, which contains the class files. + Routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ProductComponent } from './product/product.component';
+import { EventComponent } from './event/event.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    ProductComponent,
+    EventComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
+
+  ],
+  exports: [
+    [RouterModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
